1f7ddaa72687255e6c6f36c5ab75802211cb2f31
Konstantine Babuadze <konstantine.babuadze@dreamapply.com>
2019-03-21 17:31:59 +0200

    Commend Added

--- index.go
+++ index.go
@@ -6,14 +6,17 @@ import (
 	"net/http"
 )
 
+// LofInfo Logs IP and Header to the console
+// and Displays it on a website
 func LogInfo(w http.ResponseWriter, r *http.Request) {
 
-	fmt.Println("\n\nX-Forwarded-For received by the Server: "+r.Header.Get("X-Forwarded-For"))
-	fmt.Println("IP Address received by the Server: "+r.RemoteAddr)
+	fmt.Println("\n\nX-Forwarded-For received by the Server: " + r.Header.Get("X-Forwarded-For"))
+	fmt.Println("IP Address received by the Server: " + r.RemoteAddr)
+
 	w.WriteHeader(200)
-	fmt.Fprintln(w,"X-Forwarded-For received by the Server: "+r.Header.Get("X-Forwarded-For"))
-	fmt.Fprintln(w,"IP Address received by the Server: "+r.RemoteAddr)
-	
+	fmt.Fprintln(w, "X-Forwarded-For received by the Server: "+r.Header.Get("X-Forwarded-For"))
+	fmt.Fprintln(w, "IP Address received by the Server: "+r.RemoteAddr)
+
 }
 
 func main() {
@@ -21,4 +24,3 @@ func main() {
 	http.HandleFunc("/", LogInfo)
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
-
