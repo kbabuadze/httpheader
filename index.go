e2ba3151521fbb20105c6b695d4d307fa8a6a850
root <kbabuadze@yahoo.com>
2019-03-21 17:29:06 +0400

    Initial commit

---
+++ index.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func LogInfo(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Println("\n\nX-Forwarded-For received by the Server: "+r.Header.Get("X-Forwarded-For"))
+	fmt.Println("IP Address received by the Server: "+r.RemoteAddr)
+	w.WriteHeader(200)
+	fmt.Fprintln(w,"X-Forwarded-For received by the Server: "+r.Header.Get("X-Forwarded-For"))
+	fmt.Fprintln(w,"IP Address received by the Server: "+r.RemoteAddr)
+	
+}
+
+func main() {
+
+	http.HandleFunc("/", LogInfo)
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
+
